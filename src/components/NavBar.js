8fd241b2086e089feb961721cfcd4314e7e8c54b
sabricanale <sabrii.canale@gmail.com>
2022-08-27 21:06:28 -0300

    Cambios

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import logo from '../LogoRastro.png'
+
+const NavBar = () => {
+    return (
+        <div className='navBar-contenedor'>
+            
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <img src={logo} className='App-logo' alt='logo'/>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav mx-auto">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Productos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+
+
+export default NavBar
+
+
+
